defd389844712a6a6bb2a014c8789422b6b32478
Andrea Rizzi <andrrizzi@gmail.com>
2019-12-11 14:55:02 -0500

    Add DoublySelfConsistentAnalyzer

--- scripts/utils/mbarweights.py
+++ scripts/utils/mbarweights.py
@@ -1,10 +1,114 @@
 #!/usr/env/python
 
+import logging
 import os
 
 import numpy as np
 from scipy.special import logsumexp
+from yank.analyze import YankReplicaExchangeAnalyzer
 
+
+logger = logging.getLogger(__name__)
+
+
+# ============================================================
+# ANALYZER IMPLEMENTING NEW DECORRELATION METHOD
+# ============================================================
+
+class DoublySelfConsistentAnalyzer(YankReplicaExchangeAnalyzer):
+
+    def _get_equilibration_data(self, replica_energies=None, neighborhoods=None, replica_state_indices=None,
+                                equil_max_n_iterations=10):
+        """Implement the doubly self-consistent automatic detection of equilibration and statistical inefficiency.
+
+        Parameters
+        ----------
+        equil_max_n_iterations : int, optional, default=10
+            After ``equil_max_n_iterations``, if the equilibration detection
+            hasn't converged, an error is raised.
+
+        """
+        # TODO: This doesn't work with restraint reweighting.
+        import pymbar
+
+        # Check if we need to read the data.
+        if replica_energies is None or replica_state_indices is None:
+            # Case where no input is provided
+            replica_energies, _, neighborhoods, replica_state_indices = self._read_energies(
+                truncate_max_n_iterations=True)
+
+        # replica_energies[r][l][n] is the reduced potential sampled by REPLICA r
+        # and evaluated at state l at iteration n. We need to deconvolute to kln
+        # format (i.e. where the first index is state k rather than replica r).
+        n_replicas, n_states, n_iterations = replica_energies.shape
+        mbar_energies = np.empty(shape=(n_states, n_states, n_iterations), dtype=np.float64)
+        for iteration in range(n_iterations):
+            state_indices = replica_state_indices[:, iteration]
+            mbar_energies[state_indices,:,iteration] = replica_energies[:,:,iteration]
+
+        # Start iterative algorithm.
+        for iteration in range(equil_max_n_iterations):
+            logger.debug(f'Equilibration detection iteration {iteration}')
+
+            N_k = np.array([mbar_energies.shape[-1] for _ in range(mbar_energies.shape[0])])
+            logger.debug(f'    Initializing MBAR')
+            mbar = pymbar.MBAR(mbar_energies, N_k)
+            # We don't compute the covariance to make it faster.
+            logger.debug(f'    Computing MBAR free energy')
+            Deltaf_ij, _, _ = mbar.getFreeEnergyDifferences(compute_uncertainty=False, return_theta=False)
+
+            # Print the difference in free energy.
+            Deltaf = Deltaf_ij[0]
+            logger.debug(f'    Deltaf (before discarding) = {Deltaf[-1]}')
+
+            # Compute the equilibration time and statistical
+            # inefficiency of the bound and decoupled states.
+            logger.debug(f'    Computing MBAR weights')
+            mbar_log_weight_trajectories = compute_hrex_log_weight_trajectory(
+                mbar_energies, Deltaf, computed_states=[0, n_states-1], verbose=False)
+
+            # Perform automatic equilibration detection on the MBAR weight time series.
+            # The normalized correlation function is invariant under scaling factors.
+            mbar_weight_trajectories = compute_scaled_mbar_weight_trajectories(mbar_log_weight_trajectories)
+            equilibration_data = np.empty(shape=(len(mbar_weight_trajectories), 3))
+            logger.debug(f'    Running detectEquilibration')
+            for trajectory_idx, mbar_weight_trajectory in enumerate(mbar_weight_trajectories):
+                equilibration_data[trajectory_idx] = pymbar.timeseries.detectEquilibration(mbar_weight_trajectory, fast=False, nskip=1)
+
+            for state_idx in range(len(equilibration_data)):
+                t, g_t, N_eff_max = equilibration_data[state_idx]
+                logger.debug(f'    state {state_idx}: n_equil_iterations={t}, statistical_ineff={g_t}, n_effective_iter={N_eff_max}')
+
+            # Subsample mbar_energies.
+            t_max = max(equilibration_data[:,0])
+            g_t_max = max(equilibration_data[:,1])
+            uncorrelated_indices = []
+            n = 0
+            while int(round(n * g_t_max)) < mbar_energies.shape[-1] - t_max:
+                t = int(round(n * g_t_max))
+                # ensure we don't sample the same point twice
+                if (n == 0) or (t != uncorrelated_indices[n - 1]):
+                    uncorrelated_indices.append(t)
+                n += 1
+            uncorrelated_indices = np.array(uncorrelated_indices, dtype=np.int) + int(t_max)
+            n_effective_max = len(uncorrelated_indices)
+            logger.debug(f'    discarding a total of {mbar_energies.shape[-1] - n_effective_max} iterations '
+                         f'({n_effective_max} iterations remaining)')
+            mbar_energies = mbar_energies[:,:,uncorrelated_indices]
+
+            # Check if the algorithm converged.
+            if t_max == 0 and g_t_max < 2:
+                self._equilibration_data = tuple([t_max, g_t, n_effective_max])
+                logger.debug('Doubly self-consistent equilibration data:')
+                logger.debug('    number of iterations discarded to equilibration : {}'.format(t_max))
+                logger.debug('    statistical inefficiency of production region   : {}'.format(g_t))
+                logger.debug('    effective number of uncorrelated samples        : {}'.format(n_effective_max))
+
+                return t_max, g_t, n_effective_max
+
+        # If we get to this point, we've run through the
+        # maximum number of iterations without converging.
+        raise RuntimeError(f'Ran {equil_max_n_iterations} without converging doubly self-consistent analysis.')
 
 
 # =============================================================================
